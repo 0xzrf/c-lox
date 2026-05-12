dfb2238a6c7b2c74de9fdc1a3bb131b8e42d1066
Zeref <zeref18.drgnl@gmail.com>
2026-05-12 08:40:39 +0530

    added init and write chunk

---
+++ opcodes/chunk.c
@@ -0,0 +1,18 @@
+#include "chunk.h"
+#include <stdlib.h>
+
+// initializes a un-initialized chunk struct
+void init_chunk(Chunk *chunk) {
+    chunk->capacity = 0;
+    chunk->count = 0;
+    chunk->code = NULL;
+}
+
+void write_chunk(Chunk *chunk, uint8_t byte) {
+    if (chunk->capacity < chunk->count + 1) {
+        // step 1: Increase the capacity
+        // step 2: Allocate -> copy array elem to it -> make `chunk->code` point at it
+    }
+
+    chunk->code[chunk->count++] = byte;
+}
--- opcodes/chunk.h
+++ opcodes/chunk.h
@@ -3,12 +3,18 @@
 
 #include "../common.h"
 
+// The list of all the bytecodes supported by the VM
 typedef enum {
     OP_RETURN,
 } OpCode;
 
 typedef struct {
-    uint8_t* code;
+    int count; // the count of elements in use by `code`
+    int capacity; // the capacity of the dynamic array `code`
+    uint8_t* code; // the actual (dynamic)array of bytecodes
 } Chunk;
+
+void init_chunk(Chunk* chunk);
+void write_chunk(Chunk* chunk, uint8_t byte);
 
 #endif
