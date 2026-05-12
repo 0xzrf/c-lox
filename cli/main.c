b2aa6df08b58f45ed14c3a9dbb770b78a95ac426
Zeref <zeref18.drgnl@gmail.com>
2026-05-12 09:30:38 +0530

    added a MakeFile

--- cli/main.c
+++ cli/main.c
@@ -1,5 +1,11 @@
-#include "../common/common.h"
+#include "../opcodes/chunk.h"
 
 int main(int argc, const char *argv[]) {
+    Chunk chunk;
+
+    init_chunk(&chunk);
+    write_chunk(&chunk, OP_RETURN);
+    free_chunk(&chunk);
+
     return 0;
 }
