f1b6f692f65194b1d6e69cafd2710d2f90b09ff7
Zeref <zeref18.drgnl@gmail.com>
2026-05-12 10:02:05 +0530

    added disassembler

--- cli/main.c
+++ cli/main.c
@@ -1,10 +1,13 @@
 #include "../opcodes/chunk.h"
+#include "../debug/debug.h"
 
 int main(int argc, const char *argv[]) {
     Chunk chunk;
 
     init_chunk(&chunk);
     write_chunk(&chunk, OP_RETURN);
+    disassemble_chunk(&chunk, "test chunks");
+
     free_chunk(&chunk);
 
     return 0;
