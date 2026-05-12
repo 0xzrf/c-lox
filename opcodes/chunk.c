9913aa4cfa416393c2be327ae8a849f3a6bfb1f7
Zeref <zeref18.drgnl@gmail.com>
2026-05-12 08:45:28 +0530

    defined GROW_CAPACITY

--- opcodes/chunk.c
+++ opcodes/chunk.c
@@ -1,4 +1,5 @@
 #include "chunk.h"
+#include "memory.h"
 #include <stdlib.h>
 
 // initializes a un-initialized chunk struct
@@ -11,6 +12,8 @@ void init_chunk(Chunk *chunk) {
 void write_chunk(Chunk *chunk, uint8_t byte) {
     if (chunk->capacity < chunk->count + 1) {
         // step 1: Increase the capacity
+        int old_cap = chunk->capacity;
+        int new_cap = GROW_CAPACITY(old_cap);
         // step 2: Allocate -> copy array elem to it -> make `chunk->code` point at it
     }
 
