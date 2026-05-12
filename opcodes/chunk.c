5ed96b34197a37f896bd3c196cc53480b13bf10b
Zeref <zeref18.drgnl@gmail.com>
2026-05-12 14:30:03 +0530

    added get_line function to get the line

--- opcodes/chunk.c
+++ opcodes/chunk.c
@@ -1,28 +1,39 @@
+#include <stdlib.h>
+#include <stdio.h>
+
 #include "chunk.h"
 #include "../common/memory.h"
 
-#include <stdlib.h>
 
 // initializes a un-initialized chunk struct
 void init_chunk(Chunk *chunk) {
     chunk->capacity = 0;
     chunk->count = 0;
     chunk->code = NULL;
-    chunk->line = NULL;
+    chunk->lines = NULL;
+    chunk->line_count = 0;
     init_value_array(&chunk->constants);
 }
 
 void write_chunk(Chunk *chunk, uint8_t byte, int line) {
+    if (line <= 0) {
+        printf("Expected line number to be > 0");
+        exit(1);
+    }
+
     if (chunk->capacity < chunk->count + 1) {
         // step 1: Increase the capacity
         int old_cap = chunk->capacity;
         chunk->capacity = GROW_CAPACITY(old_cap);
         // step 2: Allocate -> copy array elem to it -> make `chunk->code` point at it
         chunk->code = GROW_ARRAY(uint8_t, chunk->code, old_cap, chunk->capacity);
-        chunk->line = GROW_ARRAY(int, chunk->line, old_cap, chunk->capacity);
     }
 
-    chunk->line[chunk->count] = line;
+    if (line > chunk->line_count) {
+        chunk->line_count = line;
+    }
+
+    chunk->lines[line]++;
     chunk->code[chunk->count++] = byte;
 }
 
@@ -35,4 +46,17 @@ void free_chunk(Chunk *chunk) {
 int add_constant(Chunk * chunk, Value value) {
     write_value_array(&chunk->constants, value);
     return chunk->constants.count - 1;
+}
+
+char get_line(Chunk* chunk, int instruction_ix) {
+    char prev_line;
+    int i;
+
+    for (prev_line = 1, i = 1; i <= chunk->line_count; i++) {
+        if (instruction_ix >= prev_line && instruction_ix <= prev_line + chunk->lines[i]) {
+            return i;
+        }
+    }
+
+    return -1;
 }
--- opcodes/chunk.h
+++ opcodes/chunk.h
@@ -13,7 +13,8 @@ typedef struct {
     int count; // the count of elements in use by `code`
     int capacity; // the capacity of the dynamic array `code`
     uint8_t* code; // the actual (dynamic)array of bytecodes
-    int* lines;
+    char* lines; // this will act as a key pair. So for line number 1, it will determine the instructions associated with that line
+    int line_count;
     ValueArray constants; // constants that're referred to in the bytecodes(like `1 + 2` can be converted to immediate `3`)
 } Chunk;
 
@@ -21,5 +22,6 @@ void init_chunk(Chunk*);
 void write_chunk(Chunk*,uint8_t, int);
 void free_chunk(Chunk*);
 int add_constant(Chunk*,Value);
+char get_line(Chunk*, int);
 
 #endif
