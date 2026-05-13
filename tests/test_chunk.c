f7630b0a2303b75f16eb00efedadab86591611c8
Zeref <zeref18.drgnl@gmail.com>
2026-05-14 01:10:40 +0530

    added setUp and tearDown

--- tests/test_chunk.c
+++ tests/test_chunk.c
@@ -1,18 +1,24 @@
 #include "test_chunk.h"
 
-void setUp(void) {}
-void tearDown(void) {}
+Chunk chunk;
+
+void setUp(void) {
+    init_chunk(&chunk);
+}
+void tearDown(void) {
+    free_chunk(&chunk);
+}
 
 static void test_init_chunk(void) {
-    Chunk chunk;
-
-    init_chunk(&chunk);
-
     TEST_ASSERT_EQUAL(chunk.capacity, 0);
     TEST_ASSERT_EQUAL(chunk.count, 0);
     TEST_ASSERT_EQUAL(chunk.code, NULL);
     TEST_ASSERT_EQUAL(chunk.line_count, 0);
     TEST_ASSERT_EQUAL(chunk.lines, NULL);
+}
+
+static void test_write_chunk(void) {
+
 }
 
 void run_chunk_tests(void)  {
