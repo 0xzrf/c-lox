19d7aeec4b497cd85c8be3903b11214770e347aa
Zeref <zeref18.drgnl@gmail.com>
2026-05-13 10:56:37 +0530

    added init_vm() inside the cli

--- cli/main.c
+++ cli/main.c
@@ -1,17 +1,26 @@
+#include <stdio.h>
+
 #include "../opcodes/chunk.h"
 #include "../debug/debug.h"
-#include <stdio.h>
+#include "../vm/vm.h"
+
 int main(int argc, const char *argv[]) {
+    init_vm();
+
     Chunk chunk;
 
     init_chunk(&chunk);
+
     write_chunk(&chunk, OP_RETURN, 1);
     int constant = add_constant(&chunk, 1.2);
+
     write_chunk(&chunk, OP_CONSTANT, 2);
     write_chunk(&chunk, constant, 2);
+
     disassemble_chunk(&chunk, "test chunks");
 
     free_chunk(&chunk);
+    free_vm();
 
     return 0;
 }
---
+++ vm/vm.c
@@ -0,0 +1,8 @@
+#include "vm.h"
+#include "../common/common.h"
+
+VM vm;
+
+void init_vm() {}
+
+void free_vm() {}
