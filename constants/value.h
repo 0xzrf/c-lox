32fadcbbf2caeef170698894abd9625c07e12dcf
Zeref <zeref18.drgnl@gmail.com>
2026-05-17 08:22:12 +0530

    added Obj typedef to allocate strings, fns, instances and classes

--- constants/value.h
+++ constants/value.h
@@ -3,13 +3,17 @@
 
 #include <stdbool.h>
 
-typedef enum { VAL_BOOL, VAL_NIL, VAL_NUM } ValueType;
+typedef enum { VAL_BOOL, VAL_NIL, VAL_NUM, VAL_OBJ } ValueType;
+
+typedef struct {
+} Obj;
 
 typedef struct {
   ValueType type;
   union {
     bool boolean;
     double number;
+    Obj *obj;
   } as;
 } Value;
 
@@ -22,13 +26,16 @@ typedef struct {
 #define IS_BOOL(value) ((value).type == VAL_BOOL)
 #define IS_NIL(value) ((value).type == VAL_NIL)
 #define IS_NUM(value) ((value).type == VAL_NUM)
+#define IS_OBJ(value) ((value).type == VAL_OBJ)
 
 #define AS_BOOL(value) ((value).as.boolean)
 #define AS_NUMBER(value) ((value).as.number)
+#define AS_OBJ(value) ((value).as.obj)
 
 #define BOOL_VAL(value) ((Value){VAL_BOOL, {.boolean = value}})
 #define NIL_VAL ((Value){VAL_NIL, {.number = 0}})
 #define NUMBER_VAL(value) ((Value){VAL_NUM, {.number = value}})
+#define OBJ_VAL(value) ((Value){VAL_OBJE, {.obj = value}})
 
 void init_value_array(ValueArray *);
 void write_value_array(ValueArray *, Value);
